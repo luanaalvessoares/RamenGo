3a969fb606c0813567b45401ec18ec91ef926118
Luana Alves <luanaalvessoares_@outlook.com>
2024-05-31 16:52:24 -0300

    feat: função assíncrona para requisição

---
+++ RamenGo/js/script.js
@@ -0,0 +1,20 @@
+async function fetchData(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': 'ZtVdh8XQ2U8pWI2gmZ7f796Vh8GllXoN7mr0djNf'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Falha na requisição: ' + response.statusText);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+    console.error('Erro ao fazer a requisição:', error);
+    }
+}
